9a6143fb09654edc9fce841d7f39e9c3a69a968b
SalomeMokuwa <salomemm@bu.edu>
2026-02-20 16:14:37 -0500

    user can now get ID from db

--- main.py
+++ main.py
@@ -9,38 +9,19 @@ from fastapi import HTTPException
 
 app = FastAPI()
 
-# default, returns for root link (localhost:8000/)
-# adding /docs at the end will get you a Swagger UI representation of your endpoints
-# adding /redoc gives redoc representation
-@app.get("/")
+db = []
+
+@app.get("/database")
+# shows db
 def read_root():
-    return {"Hello": "World"}
-
-# # new endpoint, responds to (localhost:8000/items/#?q=value)
-# # ? signifies beginning of query
-# # {} makes value dynamic
-# @app.get("/items/{item_id}")
-
-# # in function specify type
-# def read_item(item_id: int, q: str | None = None):
-#     return {"item_id": item_id, "q": q}
-
-# #################################
-# @app.get("/blog")
-# # query params specified in function, not endpoint
-# def index(limit: int, published: bool, sort: Optional[str] = None):
-#     if published:
-#         return {"data": f"{limit} published blogs from the db"}
-#     else:
-#        return {"data": f"{limit} unpublished blogs from the db"} 
+    return db
     
 class User(BaseModel):
     name:str
     user_id: str
 
-db = []
-
 @app.post("/user")
+# function to add user to db
 def create_user(user:User):
     # use dot notation to access base model;
     for acc in db:
@@ -52,10 +33,16 @@ def create_user(user:User):
     return db
     
 @app.get("/user/{id}")
-# automatically identifies the first input as a path parameter
-# knows the rest are query parameters
+# function to get userid from db
 def get_user(id):
-    return "hi"
+    count = 0
+    sz = len(db)
+    for acc in db:
+        if id == acc["user_id"]:
+            return acc
+        count += 1
+        if count == sz:
+            raise HTTPException(status_code=409, detail = "Account not found") 
     
 
 
