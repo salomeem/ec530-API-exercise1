0ea5fa444f4b73984f03903b955e045a36bb5994
SalomeMokuwa <salomemm@bu.edu>
2026-02-20 19:46:52 -0500

    add ability for user to see text based on ID

--- main.py
+++ main.py
@@ -40,9 +40,10 @@ class User(BaseModel):
 def create_user(user:User):
     # use dot notation to access base model;
     for acc in db:
-        print(user)
         if user.name == acc["name"]:
             raise HTTPException(status_code=409, detail = "Username already exists")
+        if user.user_id == acc["user_id"]:
+            raise HTTPException(status_code=409, detail = "ID already exists")
     user_info = {"name": user.name, "user_id": user.user_id}
     db.append(user_info)
     return db
@@ -71,13 +72,12 @@ def put_text(user_id: int, text: AddText):
             return acc
     raise HTTPException(status_code=404, detail="User not found")
     
-@app.get("/user/text")
+@app.get("/user/{user_id}/text")
 def get_text(user_id: int):
     for acc in db:
         if acc["user_id"] == user_id:
+            print(acc)
             if "text" in acc:
                 return acc["text"]
             else:
                 raise HTTPException(status_code=404, detail="Text not found")
-        else:
-            continue
